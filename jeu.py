db91fec26ae433d3b6bfb896c8a3e6e87f2c4a2b
paulluka <paul.luka@hotmail.fr>
2019-01-24 20:06:42 +0100

    update config afficher octet

--- jeu.py
+++ jeu.py
@@ -30,13 +30,38 @@ def generation(level):
 
 def verify():
 
+
 	if saisie_decimal.get() == str(generation_decimal):
-		print("c bn chakal")
+		reponse = "Correct. Félicitation !"
 		generation(new_level)
+		afficher_octet.config(text=generation_binaire)
 	else:
-		print("c ps bon ça")
+		reponse = "Hmmm... le résultat n'est pas celui la !"
 
 	saisie_decimal.delete(0, END)
+	
+
+	
+	#-----------------------------------
+	#création fenetre reponse
+	#-----------------------------------		
+
+	interface_reponse = Tk()
+	interface_reponse.geometry("250x50")
+	interface_reponse.title("Resultat !")
+
+	phrase_reponse = Label(interface_reponse, text=reponse)
+	phrase_reponse.grid(row=0, column=1)
+	
+	boutton_quitter_reponse = Button(interface_reponse, text="Recommencer", command=interface_reponse.destroy)
+	boutton_quitter_reponse.grid(row=0, column=3)
+	
+
+	
+
+	interface_reponse.mainloop()
+
+
 
 
 
@@ -45,27 +70,28 @@ def verify():
 #------------------------------------------------------------------------------------------------
 #Création de l'interface graphique																#
 #------------------------------------------------------------------------------------------------
-generation(2)
+generation(1)
 
 # Premiere fenetre interface_insertion
 interface_insertion = Tk()
-interface_insertion.geometry("300x110")
+interface_insertion.geometry("500x110")
 interface_insertion.title("Convertion d'un nombre binaire")
 
 # text de l'octet
-text_afficher_octet = Label(interface_insertion, text="converti l'octet  : ")
+text_afficher_octet = Label(interface_insertion, text="Convertir l'octet  : ")
 text_afficher_octet.grid(row=0, column=0)
 
 #afficher l'octet
-afficher_octet = Label(interface_insertion, text ="Octet")
+afficher_octet = Label(interface_insertion, text =generation_binaire)
 afficher_octet.grid(row=0, column=1)
 
 def update_level(x):
 	global new_level
 	new_level = x
+	print(new_level)
 
 #scale
-scale_text = Label(interface_insertion, text ="choisissez un niveau")
+scale_text = Label(interface_insertion, text ="Choisissez un niveau entre 1 et 3 suivant votre niveau.")
 scale_text.grid(row=1, column=0)
 
 scale_level = Scale(interface_insertion, from_=1,to=3, orient=HORIZONTAL , command= update_level)
@@ -73,7 +99,7 @@ scale_level.grid(row=1, column=1)
 
 
 #text decimal
-saisie_text_decimal = Label(interface_insertion, text="la valeur decimal est : ")
+saisie_text_decimal = Label(interface_insertion, text="La valeur decimal est : ")
 saisie_text_decimal.grid(row=2, column=0)
 
 #saisir decimal
@@ -81,12 +107,20 @@ saisie_decimal = Entry(interface_insertion)
 saisie_decimal.grid(row=2, column=1)
 
 #Boutton de validataion 
-boutton_valider = Button(interface_insertion, text="valider", command=verify)
+boutton_valider = Button(interface_insertion, text="Valider", command=verify)
 boutton_valider.grid(row=3, column=0)
+
 
 
 #boutton quitter
 boutton_quitter = Button(text="Quitter", command=interface_insertion.destroy)
 boutton_quitter.grid(row=3, column=1)
 
+scale_level.set(2)
+scale_level.set(1)
+
+
 interface_insertion.mainloop()
+
+#--------------------------------------------------------------
+
